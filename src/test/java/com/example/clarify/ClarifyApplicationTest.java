7db21212658fd4ae1fc88e5546c9ff5e96798cfe
Satyam6623165 <satyam6623165@gmail.com>
2021-11-11 13:07:48 +0530

    add postgres service to test job

--- src/test/java/com/example/clarify/ClarifyApplicationTest.java
+++ src/test/java/com/example/clarify/ClarifyApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClarifyApplicationTests {
+class ClarifyApplicationTest {
 
 	@Test
 	void contextLoads() {
