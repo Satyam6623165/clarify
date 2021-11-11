1c05d7592af27ac94a40624867f385129d49a57f
Satyam6623165 <satyam6623165@gmail.com>
2021-11-11 09:48:35 +0530

    initial commit

---
+++ src/test/java/com/example/clarify/ClarifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clarify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
